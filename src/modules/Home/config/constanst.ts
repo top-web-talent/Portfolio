c6ad542dd75dd754b453d4edaadbc49ebb7b583a
RodolfoBenjamim <rodolfobenjamim212@gmail.com>
2023-08-05 13:35:39 -0300

    completed

--- src/modules/Home/config/constanst.ts
+++ src/modules/Home/config/constanst.ts
@@ -3,4 +3,7 @@ interface MyTArray {
   [index: number]: [string, number]
 }
 
-export const skills: MyTArray = [ ["React", 10], ["Next.js", 10], ["Nuxt.js", 8], ["Gatsby", 8], ["Vue.js", 4], ["Sanity.io", 10], ["JavaScript", 10], ["TypeScript", 10], ["GraphQL", 6], ["Cypress", 9]]
+type TMyArray = [string, number][]
+
+
+export const skills: TMyArray = [ ["React", 10], ["Next.js", 10], ["Nuxt.js", 8], ["Gatsby", 8], ["Vue.js", 4], ["Sanity.io", 10], ["JavaScript", 10], ["TypeScript", 10], ["GraphQL", 6], ["Cypress", 9]]
